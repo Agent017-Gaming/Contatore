e0a5b90350551412770f9ea6363c22caa4a905f2
Agent017-Gaming <fahmida596@gmail.com>
2025-11-10 13:31:14 +0100

    did a bit of code Changes to work it around, (it still have some issues on it)

---
+++ src/Counter.java
@@ -0,0 +1,17 @@
+public class Counter {
+    private int count = 0;
+
+    public void increment() {
+        int i = 0;
+        while (i < 5000){
+            count++;
+            i++;
+        }
+        System.out.printf("%s, count: %d\n", Thread.currentThread().getName(),this.getCount());
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        Contatore c = new Contatore();
+        Counter c = new Counter();
+        CounterSynchronized cs = new CounterSynchronized();
+        
 
         // Create multiple threads to increment the counter
-        Thread t1 = new Thread(() -> c.increment());
-        Thread t2 = new Thread(() -> c.increment());
+        Thread t1 = new Thread(c::increment);
+        Thread t2 = new Thread(c::increment);
+        Thread t3 = new Thread(cs::increment);
+        Thread t4 = new Thread(cs::increment);
 
         t1.start();
         t2.start();
+        t3.start();
+        t4.start();
 
         try {
             t1.join();
             t2.join();
+            t3.join();
+            t4.join();
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.printf("Error: %s", e.getMessage());
         }
-
-        System.out.println("Final count: " + c.getCount());
     }
 
 }
